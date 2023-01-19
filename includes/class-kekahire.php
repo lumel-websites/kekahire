529b31a80ee49b31ae9afd9b03a737f570779297
puneetlumel <puneett@lumel.com>
2023-01-19 11:06:00 +0530

    Added wp footer action

--- includes/class-kekahire.php
+++ includes/class-kekahire.php
@@ -178,7 +178,7 @@ class Kekahire {
 
 		$plugin_public = new Kekahire_Public( $this->get_plugin_name(), $this->get_version() );
 
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
+		$this->loader->add_action( 'wp_footer', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 		
 		/*Add shortcode*/ 
